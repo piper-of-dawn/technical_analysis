4ac208a0f6d2d915e50257857f8ee6d71f45a333
piper-of-dawn <69080662+piper-of-dawn@users.noreply.github.com>
2021-11-10 09:43:37 +0530

    dd

--- script1.js
+++ script1.js
@@ -1,51 +1,66 @@
+Things = ["#trend","#seasonality","#residual"]
+let id = [15,16,17]
+
+for (let i = 0 ; i <= Things.length - 1; i++) {
+
+
+
 const margin = {top: 10, right: 30, bottom: 30, left: 50},
     width = 1080 - margin.left - margin.right,
-    height = 400 - margin.top - margin.bottom;
-const parseTime = d3.timeFormat("%B %d, %Y");
+    height =150 - margin.top - margin.bottom;
 
-    const svg = d3.select("#my_dataviz").append("svg").attr("width", width + margin.left + margin.right)
+
+    const svg = d3.select(Things[i]).append("svg").attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom).append("g").attr("transform", `translate(${margin.left},${margin.top})`);
 
+//=========================================/
 const x = d3.scaleTime().range([0,width]);
 const y = d3.scaleLinear().range([height, 0]);
 const xAxis = d3.axisBottom().scale(x);
 svg.append("g").attr("transform", "translate(0," + height + ")").attr("class","myXaxis")
-
 const yAxis = d3.axisLeft().scale(y);
 svg.append("g").attr("class","myYaxis")
+//=========================================/
 
-
-function quarter(array){
-  return d =>  {return d.d3.timeFormat("%Y");}
-}
+  // When reading the csv, I must format variables:
  
-raw = d3.csv("https://raw.githubusercontent.com/piper-of-dawn/technical_analysis/main/raw.csv")
+raw = d3.csv("https://raw.githubusercontent.com/piper-of-dawn/technical_analysis/main/raw.csv",  function(d){
+    return { Date : d3.timeParse("%Y-%m-%d")(d.Date)}
+  });
+
+raw = d3.csv("https://raw.githubusercontent.com/piper-of-dawn/technical_analysis/main/raw.csv",  function(d){
+    return { Date : d3.timeParse("%Y-%m-%d")(d.Date),
+             Qtr: quarter(d.Date),
+             Htpts: +d.htpts,
+             four_DMA: +d.four_DMA,
+             nine_DMA: +d.nine_DMA,
+             thirteen_DMA: +d.thirteen_DMA,
+  }
+  });
 
 
-
-
-function create (y_id){
 
 raw.then(function(data){
 
-
-
 function update(data){ 
-
+console.log(id[i])
 // Create the X axis:
-  x.domain(d3.extent(data, function(d) { return d.Date }));
+  x.domain(d3.extent(data, function(d) { return parseTime(d.Date) }));
   
-
+console.log(data, function(d) { return parseTime(d.Date) })
 svg.selectAll(".myXaxis").transition()
     .duration(3000)
     .call(xAxis);
 
 
+
 /*if(y_data == "nine"){
   console.log(d3.max(data.columns[2]))
 }*/
-var yx=data.columns[1];
-//console.log(yx)
+let xdd = id[i]
+var yx=data.columns[xdd];
+
+console.log(data.columns[0])
 
   y.domain([0, d3.max(data, function(d) { return +d[yx]})]);
   svg.selectAll(".myYaxis")
@@ -66,7 +81,7 @@ var yx=data.columns[1];
     .transition()
     .duration(3000)
     .attr("d", d3.line()
-      .x(function(d) { return x(d.Date); })
+      .x(function(d) { return x(parseTime(d.Date)); })
       .y(function(d) { return y(d[yx]); })
         .curve(d3.curveCatmullRom))
       .attr("fill", "none")
@@ -80,10 +95,10 @@ update(data);
 
    })  
 
+
 }
 
-// At the beginning, I run the update function on the first dataset:
-create(2);
+
 
 
 
